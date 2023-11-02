b91f9eff8502088df6f64a7d696f67e0e69851dc
Azure <azurehusker@gmail.com>
2023-11-01 21:09:39 -0700

    Fix a bug and add ongoing score updates for ui
    there was a bug preventing function from pulling a variable, fixed
    and added ongoing score that updates every button click

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,74 +11,81 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let choice = prompt("Make Your Selection", "");
-    let result = choice.toLowerCase();
-    return result
-}
-
 function writeDraw() {
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-
     listItem.appendChild(listText);
     listText.textContent = `You chose ${playerSelection}, and so did the computer, it's a draw!`
     list.appendChild(listItem);
 }
 
-function writeLose() {
-    console.log("EEE");
-
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
+function writeLose(computerSelection) {
+    computerScore++;
+    document.getElementById("computer-score").innerHTML = computerScore;
 
     listItem.appendChild(listText);
     listText.textContent = `You chose ${playerSelection}, and the computer chose ${computerSelection}, you Lose!`
     list.appendChild(listItem);
+
+    if (computerScore >= 5){
+        listItem.appendChild(listText);
+        listText.textContent = "Computer has 5 points, Computer WINS!";
+        list.appendChild(listItem);
+        computerScore = 0
+        playerScore = 0
+        document.getElementById("computer-score").innerHTML = computerScore;
+        document.getElementById("player-score").innerHTML = playerScore;
+    }
 }
 
-function writeWin() {
-    console.log("OOO");
-
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
+function writeWin(computerSelection) {
+    playerScore++;
+    document.getElementById("player-score").innerHTML = computerScore;
 
     listItem.appendChild(listText);
-    listText.textContent = `You chose ${playerSelection}, you Win!`
+    listText.textContent = `You chose ${playerSelection}, and the computer chose ${computerSelection}, you Win!`
     list.appendChild(listItem);
+
+    if (playerScore >= 5){
+        listItem.appendChild(listText);
+        listText.textContent = "Player has 5 points, Player WINS!";
+        list.appendChild(listItem);
+        computerScore = 0
+        playerScore = 0
+        document.getElementById("computer-score").innerHTML = computerScore;
+        document.getElementById("player-score").innerHTML = playerScore;
+    }
+
 }
 
 function playRound() {
     let computerSelection = getComputerChoice()
-    // let playerSelection = getPlayerChoice()
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
             writeDraw();
             return "Tie!";
         } else if (computerSelection === "paper") {
-            writeLose();
+            writeLose(computerSelection);
             return "You Lose!";
         } else {
-            writeWin()
+            writeWin(computerSelection)
             return "You Win!";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            writeWin();
+            writeWin(computerSelection);
             return "You Win!";
         } else if (computerSelection === "paper") {
             writeDraw();
             return "Tie!"
         } else {
-            writeLose();
+            writeLose(computerSelection);
             return "You Lose!"
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            writeLose();
+            writeLose(computerSelection);
             return "You Lose!";
         } else if (computerSelection === "paper") {
-            writeWin();
+            writeWin(computerSelection);
             return "You Win!";
         } else {
             writeDraw();
@@ -92,9 +99,16 @@ function playRound() {
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+
 const list = document.querySelector('#results')
+const listItem = document.createElement('div');
+const listText = document.createElement('span');
+
 
 let playerSelection
+let playerScore = 0
+let computerScore = 0
+
 
 rock.addEventListener('click', () => {
     playerSelection = 'rock';
